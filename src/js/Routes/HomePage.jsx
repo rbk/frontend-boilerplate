524164818e1162132942a01adb6cc9c801ca3b87
Patrick Burris <patrick@briefmedia.com>
2017-01-03 21:01:47 -0600

    remove dist from repo

--- src/js/Routes/HomePage.jsx
+++ src/js/Routes/HomePage.jsx
@@ -11,7 +11,7 @@ export default class Client extends Component {
 
   render() {
     return (
-      <section className="">
+      <section className="mx-auto px3" style={{ maxWidth: '480px' }}>
         <h1>Note list</h1>
 				<NoteList />
 
